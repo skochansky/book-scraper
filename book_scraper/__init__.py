07736e0ebf9516709100af1f011e2329d86fe6ca
skochansky <stachu.kochanski@gmail.com>
2021-10-18 23:06:39 +0200

    #Implement flask-session future

--- book_scraper/__init__.py
+++ book_scraper/__init__.py
@@ -1,10 +1,15 @@
 # Third part
 from flask import Flask
+from hashlib import md5 ### 0
+from datetime import timedelta
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
-
+    app.permanent_session_lifetime = timedelta(minutes=30)
+    encryptor = md5()
+    app.debug = True
+    app.secret_key = encryptor.digest()
     # Blueprints
     from .server import SERVER_BLUEPRINT
     app.register_blueprint(SERVER_BLUEPRINT)
